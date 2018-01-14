7e3e9fcebf33dce873c2b8db42cf27ae5cec2f3c
levieudell <34821328+levieudell@users.noreply.github.com>
2018-01-14 13:08:20 -0800

    destroy method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all
-    
+
   end
 
   def new
@@ -35,6 +35,14 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was successfully deleted"
+    redirect_to articles_path
+
   end
   private
 
